4c348e56d0c23c39fba876000349c03c48bec241
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-12-19 22:59:48 +0300

    Make all BarAggregator methods virtual

--- src/trading_structures/bar_aggregator.h
+++ src/trading_structures/bar_aggregator.h
@@ -9,18 +9,18 @@
 #include <queue>
 
 class BarAggregator {
-private:
+protected:
     std::queue<Bar> bar_queue;
 
 public:
 
     virtual void receive(const Trade &trade);
 
-    bool ready() {
+    virtual bool ready() {
         return !bar_queue.empty();
     }
 
-    Bar publish() {
+    virtual Bar publish() {
         if (ready()) {
             Bar published_bar = bar_queue.front();
             bar_queue.pop();
