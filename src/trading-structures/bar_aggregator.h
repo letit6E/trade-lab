a2722e69db765a3e8006c151c9977d06bc7fe562
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-12-20 20:30:36 +0300

    Implement static process method for BarAggregator and its heirs classes

--- src/trading-structures/bar_aggregator.h
+++ src/trading-structures/bar_aggregator.h
@@ -5,8 +5,9 @@
 #ifndef TRADE_LAB_BAR_AGGREGATOR_H
 #define TRADE_LAB_BAR_AGGREGATOR_H
 
-#include "bar.h"
 #include <queue>
+#include <fstream>
+#include "bar.h"
 
 class BarAggregator {
 protected:
@@ -31,6 +32,40 @@ public:
             throw std::runtime_error("There are no ready bars to publish.");
         }
     }
+
+    template<typename T>
+    static std::queue<Bar> process(const std::string &filepath) {
+        std::ifstream file(filepath);
+        if (!file.is_open()) {
+            throw std::runtime_error("Failed to open trade history file");
+        }
+
+        std::queue<Bar> closed_bars;
+        T aggregator;
+
+        std::string line;
+        while (std::getline(file, line)) {
+            std::istringstream iss(line);
+            Trade trade;
+            if (!(iss >> trade)) {
+                throw std::runtime_error("Failed to parse trade data");
+            }
+
+            aggregator.receive(trade);
+            if (aggregator.ready()) {
+                closed_bars.push(aggregator.publish());
+            }
+        }
+
+        file.close();
+
+        try {
+            closed_bars.push(aggregator.publish()); // force push
+        } catch (...) {}
+
+        return closed_bars;
+    }
+
 };
 
 #endif //TRADE_LAB_BAR_AGGREGATOR_H
