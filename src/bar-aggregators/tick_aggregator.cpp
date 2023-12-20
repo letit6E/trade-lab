003349edc46db0a9a6288f796f8094fc3a51f0fe
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-12-20 19:06:17 +0300

    Implement tick bar aggregator

---
+++ src/bar-aggregators/tick_aggregator.cpp
@@ -0,0 +1,36 @@
+//
+// Created by letit6E on 19.12.2023.
+//
+
+#include "tick_aggregator.h"
+
+void TickBarAggregator::receive(const Trade &trade) {
+    if (trade.timestamp < last_timestamp) {
+        throw std::invalid_argument("Bar aggregator receives trades only increasing over timestamp");
+    }
+    if (cur_bar.get_length() == bar_length) {
+        bar_queue.push(cur_bar);
+        cur_bar = Bar();
+    }
+    cur_bar.add_trade(trade);
+}
+
+bool TickBarAggregator::ready() {
+    return !bar_queue.empty() || cur_bar.get_length() == bar_length;
+}
+
+Bar TickBarAggregator::publish() {
+    if (ready()) {
+        Bar published_bar;
+        if (bar_queue.empty()) {
+            published_bar = cur_bar;
+            cur_bar = Bar();
+        } else {
+            published_bar = bar_queue.front();
+            bar_queue.pop();
+        }
+        return published_bar;
+    } else {
+        throw std::runtime_error("There are no ready bars to publish.");
+    }
+}
---
+++ src/bar-aggregators/tick_aggregator.h
@@ -0,0 +1,29 @@
+//
+// Created by letit6E on 19.12.2023.
+//
+
+#ifndef TRADE_LAB_TICK_AGGREGATOR_H
+#define TRADE_LAB_TICK_AGGREGATOR_H
+
+#include "bar_aggregator.h"
+
+class TickBarAggregator : public BarAggregator {
+private:
+    long long bar_length;
+    long long last_timestamp;
+    Bar cur_bar;
+
+public:
+    explicit TickBarAggregator(long long bar_length) : bar_length(bar_length) {
+        last_timestamp = -1;
+    }
+
+    void receive(const Trade &trade) override;
+
+    bool ready() override;
+
+    Bar publish() override;
+};
+
+
+#endif //TRADE_LAB_TICK_AGGREGATOR_H
