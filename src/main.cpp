84e4cbc7c4bfb4df3c61ea13b157eb45413ecd1d
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-12-25 02:31:59 +0300

    Migrate websocket data collector using bybit API to class

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,106 @@
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
-#include <iostream>
-#include <json/json.h>
+
 #include <functional>
+#include <string>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
 
-typedef websocketpp::client<websocketpp::config::asio_client> client;
+typedef websocketpp::client<websocketpp::config::asio_client> WebSocketClient;
+typedef WebSocketClient::message_ptr MessagePtr;
 
-class connection_metadata {
+class BybitWsClient {
 public:
-    typedef websocketpp::lib::shared_ptr<connection_metadata> ptr;
+    BybitWsClient(const std::string& uri) : m_uri(uri), m_isConnected(false), m_thread(nullptr) {}
 
-    connection_metadata(websocketpp::connection_hdl hdl, std::string uri)
-            : m_hdl(hdl), m_status("Connecting"), m_uri(uri), m_server("N/A") {}
-
-    void on_open(client* c, websocketpp::connection_hdl hdl) {
-        m_status = "Open";
-        std::cout << "> Connection opened to: " << m_uri << std::endl;
-
-        // Subscribe to trade topic
-        std::string msg = "{\"op\":\"subscribe\",\"args\":[\"trade.BTCUSDT\"]}";
-        c->send(hdl, msg, websocketpp::frame::opcode::text);
+    ~BybitWsClient() {
+        stop();
     }
 
-    void on_message(websocketpp::connection_hdl, client::message_ptr msg) {
-        if (msg->get_opcode() == websocketpp::frame::opcode::text) {
-            std::cout << "Received message: " << msg->get_payload() << std::endl;
+    void start() {
+        m_client->init_asio();
+        m_client->start_perpetual();
+
+        m_client->set_open_handler(std::bind(&BybitWsClient::on_open, this, std::placeholders::_1));
+        m_client->set_message_handler(std::bind(&BybitWsClient::on_message, this, std::placeholders::_1, std::placeholders::_2));
+        m_client->set_close_handler(std::bind(&BybitWsClient::on_close, this, std::placeholders::_1));
+
+        websocketpp::lib::error_code ec;
+        WebSocketClient::connection_ptr conn_ptr;
+
+        if (ec) {
+            std::cerr << "> Connect initialization error: " << ec.message() << std::endl;
+            return;
+        }
+
+        m_isConnected = true;
+        m_thread = new std::thread([this](){ m_client->run(); });
+    }
+
+    void stop() {
+        m_client->stop_perpetual();
+
+        if (m_thread) {
+            if (m_isConnected) {
+                websocketpp::lib::error_code ec;
+                //m_client->close(m_hdl, websocketpp::close::status::going_away, "", ec);
+            }
+            m_thread->join();
+            delete m_thread;
+            m_thread = nullptr;
         }
     }
 
-    websocketpp::connection_hdl get_hdl() const {
-        return m_hdl;
-    }
+    std::string get_trades() {
+        std::unique_lock<std::mutex> lock(m_tradeMutex);
+        m_condVar.wait(lock, [&](){ return !m_trades.empty(); });
 
-    std::string get_status() const {
-        return m_status;
+        std::string trade = m_trades.front();
+        m_trades.pop();
+
+        return trade;
     }
 
 private:
-    websocketpp::connection_hdl m_hdl;
-    std::string m_status;
+    WebSocketClient* m_client;
     std::string m_uri;
-    std::string m_server;
+    std::thread* m_thread;
+    websocketpp::connection_hdl m_hdl;
+    bool m_isConnected;
+
+    std::queue<std::string> m_trades;
+    std::mutex m_tradeMutex;
+    std::condition_variable m_condVar;
+
+    void on_open(websocketpp::connection_hdl hdl) {
+        m_hdl = hdl;
+        // Subscribe to trade channel (replace "trade.BTCUSD" with actual channel)
+        m_client->send(hdl, R"({"op":"subscribe","args":["trade.BTCUSD"]})", websocketpp::frame::opcode::text);
+    }
+
+    void on_message(websocketpp::connection_hdl, MessagePtr message) {
+        std::lock_guard<std::mutex> lock(m_tradeMutex);
+        m_trades.push(message->get_payload());
+        m_condVar.notify_one();
+    }
+
+    void on_close(websocketpp::connection_hdl) {
+        m_isConnected = false;
+    }
 };
 
-int main(int argc, char* argv[]) {
-    client c;
+int main() {
+    BybitWsClient wsClient("wss://stream.bybit.com/realtime");
+    wsClient.start();
 
-    std::string uri = "wss://stream.bybit.com/realtime_public"; // api key needed
-
-    try {
-        c.set_access_channels(websocketpp::log::alevel::all);
-        c.clear_access_channels(websocketpp::log::alevel::frame_payload);
-
-        c.init_asio();
-
-        sockaddr metadata = sockaddr();
-        c.set_open_handler(bind(&connection_metadata::on_open, &metadata, &c, ::_1));
-        c.set_message_handler(bind(&connection_metadata::on_message, &metadata, ::_1, ::_2));
-
-        websocketpp::lib::error_code ec;
-        client::connection_ptr con = c.get_connection(uri, ec);
-
-        if (ec) {
-            std::cout << "> Connect initialization error: " << ec.message() << std::endl;
-            return -1;
-        }
-
-        c.connect(con);
-        c.run();
-    } catch (websocketpp::exception const & e) {
-        std::cout << e.what() << std::endl;
-    } catch (...) {
-        std::cout << "other exception" << std::endl;
+    // Get trades in a loop for demonstration purposes
+    for (int i = 0; i < 10; ++i) {
+        std::string trade = wsClient.get_trades();
+        std::cout << "Trade: " << trade << std::endl;
     }
+
+    wsClient.stop();
     return 0;
 }
