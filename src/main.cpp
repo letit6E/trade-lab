f5c19aab17fd5253184243d9ed67836ece3a7d29
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-12-24 17:23:06 +0300

    Implement websocket data collector using bybit API

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,77 @@
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
 #include <iostream>
-#include "trading-structures/trade.h"
+#include <json/json.h>
+#include <functional>
 
-using namespace std;
+typedef websocketpp::client<websocketpp::config::asio_client> client;
 
-static const char *const HEADER = "\nTrade-lab © 2023 Rustam Shangareev.\n\n";
-static const char *const USAGE = "Usage:\n\ttrade-lab <data>\n\nDescription:\n\tCreates the trade class,\n\tand reports its inside\n";
+class connection_metadata {
+public:
+    typedef websocketpp::lib::shared_ptr<connection_metadata> ptr;
 
-int main(int argc, const char* argv[]) {
-    if (argc != 2) {
-        printf("%s%s", HEADER, USAGE);
-        return 0;
+    connection_metadata(websocketpp::connection_hdl hdl, std::string uri)
+            : m_hdl(hdl), m_status("Connecting"), m_uri(uri), m_server("N/A") {}
+
+    void on_open(client* c, websocketpp::connection_hdl hdl) {
+        m_status = "Open";
+        std::cout << "> Connection opened to: " << m_uri << std::endl;
+
+        // Subscribe to trade topic
+        std::string msg = "{\"op\":\"subscribe\",\"args\":[\"trade.BTCUSDT\"]}";
+        c->send(hdl, msg, websocketpp::frame::opcode::text);
     }
 
-    Trade trade;
+    void on_message(websocketpp::connection_hdl, client::message_ptr msg) {
+        if (msg->get_opcode() == websocketpp::frame::opcode::text) {
+            std::cout << "Received message: " << msg->get_payload() << std::endl;
+        }
+    }
 
-    std::istringstream iss(argv[1]);
-    iss >> trade;
+    websocketpp::connection_hdl get_hdl() const {
+        return m_hdl;
+    }
 
-    std::cout << trade << std::endl;
+    std::string get_status() const {
+        return m_status;
+    }
 
+private:
+    websocketpp::connection_hdl m_hdl;
+    std::string m_status;
+    std::string m_uri;
+    std::string m_server;
+};
+
+int main(int argc, char* argv[]) {
+    client c;
+
+    std::string uri = "wss://stream.bybit.com/realtime_public"; // api key needed
+
+    try {
+        c.set_access_channels(websocketpp::log::alevel::all);
+        c.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+        c.init_asio();
+
+        sockaddr metadata = sockaddr();
+        c.set_open_handler(bind(&connection_metadata::on_open, &metadata, &c, ::_1));
+        c.set_message_handler(bind(&connection_metadata::on_message, &metadata, ::_1, ::_2));
+
+        websocketpp::lib::error_code ec;
+        client::connection_ptr con = c.get_connection(uri, ec);
+
+        if (ec) {
+            std::cout << "> Connect initialization error: " << ec.message() << std::endl;
+            return -1;
+        }
+
+        c.connect(con);
+        c.run();
+    } catch (websocketpp::exception const & e) {
+        std::cout << e.what() << std::endl;
+    } catch (...) {
+        std::cout << "other exception" << std::endl;
+    }
     return 0;
 }
