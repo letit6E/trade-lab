a7d7d47f68beb585144b89e6a3349a8cac6beb73
Rustam Shangareev <rustam.letitbe@gmail.com>
2024-01-25 11:15:08 +0300

    Add target link libraries in src/CMakeLists.txt

---
+++ src/collectors/exchange.cpp
@@ -0,0 +1,57 @@
+//
+// Created by letit6E on 13.01.2024.
+//
+
+#include "exchange.h"
+
+Exchange::Exchange(std::string name, const std::string &http_host)
+    : m_name(std::move(name)) {
+    init_http(http_host);
+}
+
+void Exchange::init_http(std::string const &host) {
+    const auto results{resolver.resolve(host, "443")};
+    get_lowest_layer(stream).connect(results);
+    // Set SNI Hostname (many hosts need this to handshake successfully)
+    if (!SSL_set_tlsext_host_name(stream.native_handle(), host.c_str())) {
+        boost::system::error_code ec{static_cast<int>(::ERR_get_error()),
+                                     boost::asio::error::get_ssl_category()};
+        throw boost::system::system_error{ec};
+    }
+    stream.handshake(ssl::stream_base::client);
+}
+
+void Exchange::init_webSocket(std::string const &host, std::string const &port,
+                              const char *p = "") {
+    // Set SNI Hostname (many hosts need this to handshake successfully)
+    if (!SSL_set_tlsext_host_name(ws.next_layer().native_handle(),
+                                  host.c_str()))
+        throw beast::system_error(
+            beast::error_code(static_cast<int>(::ERR_get_error()),
+                              net::error::get_ssl_category()),
+            "Failed to set SNI Hostname");
+    auto const results = resolver_webSocket.resolve(host, port);
+    net::connect(ws.next_layer().next_layer(), results.begin(), results.end());
+    ws.next_layer().handshake(ssl::stream_base::client);
+
+    ws.handshake(host, p);
+}
+
+void Exchange::read_Socket() { ws.read(buffer); }
+
+bool Exchange::is_socket_open() {
+    if (ws.is_open()) return true;
+    return false;
+}
+
+void Exchange::write_Socket(const std::string &text) {
+    ws.write(net::buffer(text));
+}
+
+std::string Exchange::get_socket_data() {
+    return beast::buffers_to_string(buffer.data());
+}
+
+void Exchange::buffer_clear() { buffer.clear(); }
+
+void Exchange::webSocket_close() { ws.close(websocket::close_code::none); }
---
+++ src/collectors/exchange.h
@@ -0,0 +1,60 @@
+//
+// Created by letit6E on 13.01.2024.
+//
+
+#ifndef TRADE_LAB_EXCHANGE_H
+#define TRADE_LAB_EXCHANGE_H
+
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/ssl.hpp>
+
+namespace net = boost::asio;
+namespace ssl = net::ssl;
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+
+using tcp = net::ip::tcp;
+using Request = http::request<http::string_body>;
+using Stream = beast::ssl_stream<beast::tcp_stream>;
+using Response = http::response<http::dynamic_body>;
+
+class Exchange {
+   public:
+    Exchange(std::string name, const std::string &http_host);
+
+    void init_http(std::string const &host);
+    void init_webSocket(std::string const &host, std::string const &port,
+                        const char *p);
+
+    void read_Socket();
+
+    bool is_socket_open();
+
+    void write_Socket(const std::string &text);
+
+    std::string get_socket_data();
+
+    void buffer_clear();
+
+    void webSocket_close();
+
+   private:
+    std::string m_name;
+    net::io_context ioc;
+    ssl::context ctx{ssl::context::tlsv12_client};
+    tcp::resolver resolver{ioc};
+    Stream stream{ioc, ctx};
+    std::string m_web_socket_host;
+    std::string m_web_socket_port;
+    beast::flat_buffer buffer;
+    net::io_context ioc_webSocket;
+    ssl::context ctx_webSocket{ssl::context::tlsv12_client};
+    tcp::resolver resolver_webSocket{ioc_webSocket};
+    websocket::stream<beast::ssl_stream<tcp::socket>> ws{ioc_webSocket,
+                                                         ctx_webSocket};
+};
+
+#endif  // TRADE_LAB_EXCHANGE_H
